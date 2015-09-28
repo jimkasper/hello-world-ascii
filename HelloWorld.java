fc827a45b7b43d56f477c799a06bbbbad0932fd9
Jim Kasper <jimkasper@Jims-MacBook-Pro.local>
2015-09-28 16:46:50 -0400

    First Commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main (String[] args){
+		System.out.println(" +-+-+-+-+-+ +-+-+-+-+-+-+");
+		System.out.println(" |H|E|L|L|O| |W|O|R|L|D|!|");
+		System.out.println(" +-+-+-+-+-+ +-+-+-+-+-+-+");
+
+	}
+}
